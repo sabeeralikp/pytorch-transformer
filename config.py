d76b92d16fc05eba53c342e9a7c15e8c6a88cd53
Sabeeralikp <Sabeeralikp8@gmail.com>
2024-02-04 13:36:40 +0530

    config added

--- config.py
+++ config.py
@@ -1,26 +1,29 @@
 from pathlib import Path
 
+
 def get_config():
     return {
-        "batch_size": 8,
-        "num_epochs": 20,
+        "batch_size": 40,
+        "num_epochs": 30,
         "lr": 10**-4,
-        "seq_len": 350,
+        "seq_len": 400,
         "d_model": 512,
-        "datasource": 'opus_books',
-        "lang_src": "en",
-        "lang_tgt": "it",
+        "datasource": "ai4bharat/samanantar",
+        "lang_src": "ml",
+        "lang_tgt": "en",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
         "tokenizer_file": "tokenizer_{0}.json",
-        "experiment_name": "runs/tmodel"
+        "experiment_name": "runs/tmodel",
     }
+
 
 def get_weights_file_path(config, epoch: str):
     model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path(".") / model_folder / model_filename)
+
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
