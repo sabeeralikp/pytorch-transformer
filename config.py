c477966640ec83463b7089153471caf801e58cdc
Sabeeralikp <Sabeeralikp8@gmail.com>
2024-02-04 13:36:46 +0530

    config

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 40,
-        "num_epochs": 30,
+        "batch_size": 20,
+        "num_epochs": 10,
         "lr": 10**-4,
         "seq_len": 400,
         "d_model": 512,
